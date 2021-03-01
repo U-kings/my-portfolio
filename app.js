c94c3188420be33c8f547756fed4970d443fcf17
uche <kingsleyigbokwe909@gmail.com>
2021-03-01 23:14:30 +0100

    made some new changes

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 
 const path = require('path');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('build'));
     app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     })
 }
 
