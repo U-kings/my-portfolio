9997a906811ebbafbbee0bdce6f973a734167611
uche <kingsleyigbokwe909@gmail.com>
2021-03-03 10:41:21 +0100

    new changes to about section

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -13,26 +13,29 @@ export const AboutContent = styled.div`
 `;
 export const AboutH3 = styled.h3`
     font-size: 2rem;
-    margin: 0;
+    margin: 0 1rem;
 `;
 export const AboutP = styled.p`
     font-size: 1.2rem;
     font-weight: 500;
-    height: 20rem;
-    overflow: hidden;
+    /* height: 20rem; */
+    /* overflow: hidden; */
+    padding: .5rem 1rem;
+    background-color: rgba(255, 255, 0, .9);
     
     @media screen and (max-width: 768px){
-        width: 100%;
-        height: 15rem;
+        height: 25rem;
         margin-bottom: 2rem;
+        font-size: 1.15rem;
     }
 `;
 
 export const ContextBox = styled.div`
         /* display: flex; */
-        flex: 45%;
+        flex: 1;
         overflow: hidden;
-        margin: 1rem;
+        margin: auto 1rem;
+        padding: 1rem 0;
 
         @media screen and (max-width: 768px){
             flex: 100%;
@@ -41,13 +44,14 @@ export const ContextBox = styled.div`
 `;
 export const ImageContainer = styled.div`
     background-image: linear-gradient(to right, rgba(0,0,0,.7),rgba(0,0,0,.1)), url(${imgBg});
-    min-height: 20rem;
-    /* min-width: 10rem; */
+    min-height: 24rem;
     background-size: cover;
-    transition: .4s ease-in-out;
+    transition: .3s ease-in-out;
+        transform: scale(1.1);
+    
 
     &:hover{
         /* -webkit-transform: scale(1.1) rotate(45deg); */
-        transform: scale(1.1);
+        transform: scale(1.2);
     }
 `;
